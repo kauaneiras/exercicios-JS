a9c30fabef988c6a07a4741b9fd14a8c798d3fe7
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-03-27 13:36:21 -0300

    Create DrivenCondicionalTemNegativoOuNao.js

---
+++ DrivenCondicionalTemNegativoOuNao.js
@@ -0,0 +1,9 @@
+// Tem negativou ou tudo positivo
+function tudoPositivo(num1, num2, num3) {
+if (num1 < 0 || num2 < 0 || num3 < 0){
+    return "Tem negativo!"
+    }
+else {
+    return "Tudo positivo!"
+    }
+}
