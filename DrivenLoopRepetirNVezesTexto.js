31bc31fd2dc255b5b5bea372668f5ff543cd556b
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-03-27 13:37:53 -0300

    Create DrivenLoopRepetirNVezesTexto.js

---
+++ DrivenLoopRepetirNVezesTexto.js
@@ -0,0 +1,11 @@
+// repertir texto n vezes
+function multiplica(texto, repeticoes) {
+  // Escreva aqui seu código
+  let resultado = "";
+  
+  for (let i = 0; i < repeticoes; i++){
+    resultado += texto;
+  }
+  
+  return resultado;
+}
