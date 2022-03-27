19560ff47facbd69cfc5aeac068b219bba5cccc5
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-03-27 13:37:15 -0300

    Create DrivenLoopRepetir10VezesTexto.js

---
+++ DrivenLoopRepetir10VezesTexto.js
@@ -0,0 +1,9 @@
+// repetir texto 10 vezes
+function vezes10(texto) {
+  // Escreva aqui seu código
+  let resultado = "";
+  for (let i = 0; i < 10; i++) {
+    resultado += texto;
+  }
+  return resultado
+}
